8e3c0cf5dfd72412dadd01f3d8b0ddd4b29f7f5f
Solace <fukuman23@gmail.com>
2024-08-07 21:39:38 +0800

    new invoices

--- src/app/landlord/billing/invoices/new/[id]/page.tsx
+++ src/app/landlord/billing/invoices/new/[id]/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import PrimaryButton from '@/components/globals/PrimaryButton'
 import {
     Button,
     Card,
@@ -6,11 +7,14 @@ import {
     Input,
     Typography,
     Switch,
+    Select,
+    Option,
 } from '@material-tailwind/react'
+import Link from 'next/link'
 import { useState } from 'react'
 const NewInvoice = () => {
-
     const [draft, setDraft] = useState(false)
+    const [proforma, setProforma] = useState(false)
     return (
         <div className="space-y-8">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
@@ -30,15 +34,56 @@ const NewInvoice = () => {
             <Card className="max-w-4xl">
                 <CardBody className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <div className="space-y-4">
-                        {!draft ? <Input label="Invoice Number" type="number" /> : ''}
-                        <Switch label="Draft invoice?" color="blue" ripple={true} onChange={() => setDraft(!draft)} />
+                        {!draft ? (
+                            <Input
+                                label="Invoice Number"
+                                type="number"
+                                required
+                            />
+                        ) : (
+                            ''
+                        )}
+                        <Switch
+                            label="Draft invoice?"
+                            color="blue"
+                            ripple={true}
+                            onChange={() => setDraft(!draft)}
+                        />
+
+                        <Select
+                            label={
+                                !proforma
+                                    ? 'Proforma Invoice Template'
+                                    : 'Invoice Template'
+                            }
+                            required>
+                            <Option>sdf</Option>
+                        </Select>
+
+                        <Switch
+                            label="Proforma invoice?"
+                            color="blue"
+                            ripple={true}
+                            onChange={() => setProforma(!proforma)}
+                        />
+                    </div>
+
+                    <div>
+                        <Input type="date" label="Created date" required />
                     </div>
                 </CardBody>
             </Card>
 
             <section className="flex gap-2 mt-4 ">
-                <Button color="blue">Save</Button>
-                <Button variant="text">Cancel</Button>
+                <Link href="/landlord/billing/invoices">
+                    <PrimaryButton
+                        children="Cancel"
+                        variant="text"
+                        color="black"
+                    />
+                </Link>
+                <PrimaryButton children="Save" color="gray" />
+                <PrimaryButton children="Send and Save" />
             </section>
         </div>
     )
